89bfc92a6cdd125c846dda4fe553c7b2886ee84a
ashabelnikov <ashabelnikov@c1dae7de-a428-49f4-85d8-50b65f41d01d>
2008-08-30 19:26:55 +0000

    CKPS module refactoring, Knock signal processor functions API has been added.
    git-svn-id: http://svn2.assembla.com/svn/secu3app@32 c1dae7de-a428-49f4-85d8-50b65f41d01d

--- ckps.c
+++ ckps.c
@@ -22,17 +22,17 @@
 
 typedef struct
 {
-  unsigned char  ckps_new_engine_cycle_happen:1;      //флаг синхронизации с вращением
-  unsigned char  ckps_returned_to_gap_search:1;       //признак того что были отсчитаны все зубья и КА вновь был переведен в режим поиска синхрометки
   unsigned char  ckps_error_flag:1;                   //признак ошибки ДПКВ, устанавливается в прерывании от ДПКВ, сбрасывается после обработки
-  unsigned char  ckps_is_initialized_half_turn_period23:1;
-  unsigned char  ckps_delay_prepared:1;
+  unsigned char  ckps_is_valid_half_turn_period:1;
+  unsigned char  ckps_delay_prepared14:1;
+  unsigned char  ckps_delay_prepared23:1;
+  unsigned char  ckps_is_synchronized:1;
+  unsigned char  ckps_new_engine_cycle_happen:1;      //флаг синхронизации с вращением
   unsigned char  ckps_gap_occured:1;
-}ckps_flags;
+}CKPSFLAGS;
 
 typedef struct
 {
-  unsigned char sm_state;                   //текущее состояние конечного автомата (КА) 
   unsigned int  icr_prev;                   //предыдущее значение регистра захвата
   unsigned int  period_curr;                //последнй измеренный межзубный период
   unsigned int  period_prev;                //предыдущее значение межзубного периода
@@ -42,29 +42,38 @@ typedef struct
   unsigned char ignition_pulse_cogs_14;     //отсчитывает зубья импульса зажигания для цилиндров 1-4
   unsigned char ignition_pulse_cogs_23;     //отсчитывает зубья импульса зажигания для цилиндров 2-3
   unsigned int  half_turn_period;           //хранит последнее измерение времени прохождения n зубьев  
-  signed   int  ignition_dwell_angle;       //требуемый УОЗ * ANGLE_MULTIPLAYER
-  signed   int  ignition_dwell_angle_buffered;
-  unsigned char ignition_cogs;
-  unsigned int  angle_btdc;
+  signed   int  advance_angle;              //требуемый УОЗ * ANGLE_MULTIPLAYER
+  signed   int  advance_angle_buffered;
+  unsigned char ignition_cogs;              //кол-во зубьев определяющее длительность импульсов запуска коммутаторов
+  unsigned char cogs_latch14;
+  unsigned char cogs_latch23;
+  unsigned char cogs_btdc14;
+  unsigned char cogs_btdc23;
+  unsigned char starting_mode;
 }CKPSSTATE;
  
 CKPSSTATE ckps;
 
 //размещаем в свободных регистрах ввода/вывода
-__no_init volatile ckps_flags f1@0x22;
+__no_init volatile CKPSFLAGS flags@0x22;
 
 //инициализирет структуру данных/состояния ДПКВ и железо на которое он мапится 
 void ckps_init_state(void)
 {
-  ckps.sm_state = 0;
   ckps.cog = 0;
   //при первом же прерывании будет сгенерирован конец импульса запуска зажигания для цилиндров 1-4
   ckps.ignition_pulse_cogs_14 = 128; 
   ckps.ignition_pulse_cogs_23 = 128; 
   ckps.half_turn_period = 0xFFFF;                 
-  ckps.ignition_dwell_angle = 0;
-  ckps.ignition_dwell_angle_buffered = 0;
-
+  ckps.advance_angle = 0;
+  ckps.advance_angle_buffered = 0;
+  ckps.starting_mode = 0;
+  
+  flags.ckps_error_flag = 0;
+  flags.ckps_new_engine_cycle_happen = 0;
+  flags.ckps_gap_occured = 0;
+  flags.ckps_is_synchronized = 0;
+  
   //OC1А(PD5) и OC1В(PD4) должны быть сконфигурированы как выходы
   DDRD|= (1<<DDD5)|(1<<DDD4); 
 
@@ -82,7 +91,7 @@ void ckps_init_state(void)
 void ckps_set_dwell_angle(signed int angle)
 {
   __disable_interrupt();    
-  ckps.ignition_dwell_angle_buffered = angle;
+  ckps.advance_angle_buffered = angle;
   __enable_interrupt();                
 }
 
@@ -120,8 +129,12 @@ void ckps_set_cogs_btdc(unsigned char cogs_btdc)
   unsigned char _t;
   _t=__save_interrupt();
   __disable_interrupt();
-  ckps.angle_btdc = ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * (cogs_btdc - 1); 
-   __restore_interrupt(_t);
+  //11 зубьев = 66 град. до в.м.т. 
+  ckps.cogs_latch14 = cogs_btdc - 11;
+  ckps.cogs_latch23 = cogs_btdc + 19;
+  ckps.cogs_btdc14  = cogs_btdc;
+  ckps.cogs_btdc23  = cogs_btdc + 30;
+  __restore_interrupt(_t);
 }
 
 //устанавливает длительность импульса зажигания в зубьях
@@ -130,28 +143,28 @@ void ckps_set_ignition_cogs(unsigned char cogs)
   unsigned char _t;
   _t=__save_interrupt();
   __disable_interrupt();
-  ckps.ignition_cogs = cogs - 1;
+  ckps.ignition_cogs = cogs;
   __restore_interrupt(_t);
 }
 
 unsigned char ckps_is_error(void)
 {
- return f1.ckps_error_flag;
+ return flags.ckps_error_flag;
 }
 
 void ckps_reset_error(void)
 {
- f1.ckps_error_flag = 0;
+ flags.ckps_error_flag = 0;
 }
 
 //эта функция возвращает 1 если был новый цикл зажигания и сразу сбрасывает событие!
 unsigned char ckps_is_cycle_cutover_r()
 {
  unsigned char result;
-  __disable_interrupt();
- result = f1.ckps_new_engine_cycle_happen;
- f1.ckps_new_engine_cycle_happen = 0;
-  __enable_interrupt();                
+ __disable_interrupt();
+ result = flags.ckps_new_engine_cycle_happen;
+ flags.ckps_new_engine_cycle_happen = 0;
+ __enable_interrupt();                
  return result;
 }
 
@@ -159,10 +172,10 @@ unsigned char ckps_is_cycle_cutover_r()
 unsigned char ckps_is_rotation_cutover_r(void)
 {
  unsigned char result;
-  __disable_interrupt();
- result = f1.ckps_gap_occured;
- f1.ckps_gap_occured = 0;
-  __enable_interrupt();                
+ __disable_interrupt();
+ result = flags.ckps_gap_occured;
+ flags.ckps_gap_occured = 0;
+ __enable_interrupt();                
  return result;
 }
 
@@ -171,9 +184,9 @@ __interrupt void timer1_compa_isr(void)
 {
  //линия в высоком уровне, теперь настраиваем линию на переход в низкий уровень по следующему событию.
  //Начинаем отсчет длительности импульса по зубъям  
-  TCCR1A&= (~(1<<COM1A0));
-  TCCR1A|= (1<<COM1A1);   
-  ckps.ignition_pulse_cogs_23 = 0;
+ TCCR1A&= (~(1<<COM1A0));
+ TCCR1A|= (1<<COM1A1);   
+ ckps.ignition_pulse_cogs_23 = 0;
 }
 
 #pragma vector=TIMER1_COMPB_vect
@@ -181,10 +194,27 @@ __interrupt void timer1_compb_isr(void)
 {
  //линия в высоком уровне, теперь настраиваем линию на переход в низкий уровень по следующему событию.  
  //Начинаем отсчет длительности импульса по зубъям
-  TCCR1A&= (~(1<<COM1B0));
-  TCCR1A|= (1<<COM1B1);   
-  ckps.ignition_pulse_cogs_14 = 0;
+ TCCR1A&= (~(1<<COM1B0));
+ TCCR1A|= (1<<COM1B1);   
+ ckps.ignition_pulse_cogs_14 = 0;
 }
+
+//сбрасывает флаг прерывания, включает режим установки линии B в высокий уровень при совпадении
+#define prepare_channel14(value)     \
+  {                                  \
+   OCR1B = value;                    \
+   SETBIT(TIFR,OCF1B);               \
+   TCCR1A|= (1<<COM1B1)|(1<<COM1B0); \
+   }
+
+//сбрасывает флаг прерывания, включает режим установки линии A в высокий уровень при совпадении
+#define prepare_channel23(value)     \
+  {                                  \
+   OCR1A = value;                    \
+   SETBIT(TIFR,OCF1A);               \
+   TCCR1A|= (1<<COM1A1)|(1<<COM1A0); \
+  }
+
 
 //прерывание по захвату таймера 1 (вызывается при прохождении очередного зуба)
 #pragma vector=TIMER1_CAPT_vect
@@ -193,140 +223,140 @@ __interrupt void timer1_capt_isr(void)
   unsigned int diff;
  
   ckps.period_curr = GetICR() - ckps.icr_prev;
-  
-  //конечный автомат для синхронизации, измерения скорости вращения коленвала, запуска зажигания в нужное время  
-  switch(ckps.sm_state)
+
+  //при старте двигателя, пропускаем определенное кол-во зубьев для инициализации 
+  //памяти предыдущих периодов. Затем ищем синхрометку.
+  if (!flags.ckps_is_synchronized)
   {
-   case 0://----------------пусковой режим (пропускаем несколько зубов)------------------- 
-     f1.ckps_gap_occured = 0;
-     if (ckps.cog >= CKPS_ON_START_SKIP_COGS) 
-      {
-       ckps.sm_state = 1;
-       f1.ckps_returned_to_gap_search = 0;
-       f1.ckps_is_initialized_half_turn_period23 = 0;
-      }
-     break;
-
-   case 1://-----------------поиск синхрометки--------------------------------------------
-     if (ckps.period_curr > CKPS_GAP_BARRIER(ckps.period_prev)) 
-     {
-      f1.ckps_gap_occured = 1; //устанавливаем событие нахождения синхрометки
-
-      if (f1.ckps_returned_to_gap_search)
-      {
-       if ((ckps.cog != 58))
-         f1.ckps_error_flag = 1; //ERROR             
-       f1.ckps_returned_to_gap_search = 0;
-      }
-
-      ckps.cog = 0;
-      ckps.sm_state = 2;
-      ckps.ignition_pulse_cogs_14+=2;
-      ckps.ignition_pulse_cogs_23+=2;
-      f1.ckps_delay_prepared = 0;
-    
-      //начинаем отсчет угла опережения
-      ckps.current_angle = ckps.angle_btdc;
-      ckps.ignition_dwell_angle = ckps.ignition_dwell_angle_buffered;
-     }
-     break;
-
-   case 2: //--------------реализация УОЗ для 1-4-----------------------------------------
-     //прошел зуб - угол до в.м.т. уменьшился на 6 град.
-     ckps.current_angle-= ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG);
-
-     if (!f1.ckps_delay_prepared)
-     {
-      diff = ckps.current_angle - ckps.ignition_dwell_angle;
-      if (diff <= (ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * 2))
-      {
-       //до запуска зажигания осталось отсчитать меньше 2-x зубов. Необходимо подготовить модуль сравнения
-       OCR1B = GetICR() + ((unsigned long)diff * (ckps.period_curr)) / ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG);  
-     
-       //сбрасываем флаг прерывания, включаем режим установки линии B в высокий уровень при совпадении
-       SETBIT(TIFR,OCF1B);
-       TCCR1A|= (1<<COM1B1)|(1<<COM1B0);     
-       ckps.ignition_pulse_cogs_14 = 0;   
-       f1.ckps_delay_prepared = 1;
-      }
-     }
-
-     if (ckps.cog==2) //диаметральный зуб завершения измерения периода вращения для 2-3
-     {
-      //если было переполнение то устанавливаем максимально возможное время
-      ckps.half_turn_period = ((ckps.period_curr > 1250) || !f1.ckps_is_initialized_half_turn_period23) 
-                              ? 0xFFFF : (GetICR() - ckps.measure_start_value);             
-      ckps.measure_start_value = GetICR();
-      f1.ckps_new_engine_cycle_happen = 1;      //устанавливаем событие цикловой синхронизации 
-      adc_begin_measure();                 //запуск процесса измерения значений аналоговых входов        
-     }
-
-     if (ckps.cog == 30) //переход в режим реализации УОЗ для 2-3
-     {
-      //начинаем отсчет угла опережения
-      ckps.sm_state = 3;
-      f1.ckps_delay_prepared = 0;
-      ckps.current_angle = ckps.angle_btdc;      
-      ckps.ignition_dwell_angle = ckps.ignition_dwell_angle_buffered;
-     }
-
-     if (ckps.period_curr > 12500) //обороты опустилисть ниже нижнего порога - двигатель остановился
-       ckps.sm_state = 0;
-
-     break;
-
-   case 3: //--------------реализация УОЗ для 2-3-----------------------------------------
-     //прошел зуб - угол до в.м.т. уменьшился на 6 град.
-     ckps.current_angle-= ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG);
-
-     if (!f1.ckps_delay_prepared)
-     {
-      diff = ckps.current_angle - ckps.ignition_dwell_angle;
-      if (diff <= (ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * 2))
-      {
-       //до запуска зажигания осталось отсчитать меньше 2-x зубов. Необходимо подготовить модуль сравнения
-       OCR1A = GetICR() + ((unsigned long)diff * (ckps.period_curr)) / ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG);    
-
-       //сбрасываем флаг прерывания, включаем режим установки линии А в высокий уровень при совпадении
-       SETBIT(TIFR,OCF1A);
-       TCCR1A|= (1<<COM1A1)|(1<<COM1A0);      
-       ckps.ignition_pulse_cogs_23 = 0;   
-       f1.ckps_delay_prepared = 1;
-      }
-     }
-
-     if (ckps.cog == 32) //диаметральный зуб завершения измерения периода вращения для 1-4
-     {
-      //если было переполнение то устанавливаем максимально возможное время
-      ckps.half_turn_period = (ckps.period_curr > 1250) ? 0xFFFF : (GetICR() - ckps.measure_start_value);             
-      ckps.measure_start_value = GetICR();    
-      f1.ckps_new_engine_cycle_happen = 1;      //устанавливаем событие цикловой синхронизации 
-      f1.ckps_is_initialized_half_turn_period23 = 1;
-      adc_begin_measure();                 //запуск процесса измерения значений аналоговых входов
-     } 
-
-     if (ckps.cog > 55) //переход в режим поиска синхрометки
-     {
-      ckps.sm_state = 1;
-      f1.ckps_returned_to_gap_search = 1; 
-      f1.ckps_delay_prepared = 0;
-     }
-
-     if (ckps.period_curr > 12500) //обороты опустилисть ниже нижнего порога - двигатель остановился
-       ckps.sm_state = 0;
-
-     break;
+   switch(ckps.starting_mode)
+   {
+   case 0:
+    /////////////////////////////////////////
+    flags.ckps_gap_occured = 0;
+    flags.ckps_is_valid_half_turn_period = 0;
+    /////////////////////////////////////////
+    if (ckps.cog >= CKPS_ON_START_SKIP_COGS) 
+     ckps.starting_mode = 1;
+    break;
+   case 1:
+    if (ckps.period_curr > CKPS_GAP_BARRIER(ckps.period_prev)) 
+    {
+     flags.ckps_is_synchronized = 1;
+     ckps.cog = 1; //1-й зуб
+     goto synchronized_enter;
+    }
+    break;
+   }
+   ckps.icr_prev = GetICR();
+   ckps.period_prev = ckps.period_curr;  
+   ckps.cog++; 
+   return;
   }
-   
+
+  //каждый период проверяем на синхрометку, и если после обнаружения синхрометки
+  //оказалось что кол-во зубьев неправильное, то устанавливаем признак ошибки.
+  if (ckps.period_curr > CKPS_GAP_BARRIER(ckps.period_prev)) 
+  {
+   /////////////////////////////////////////
+   flags.ckps_gap_occured = 1; //устанавливаем событие нахождения синхрометки
+   /////////////////////////////////////////
+   if ((ckps.cog != 59))
+     flags.ckps_error_flag = 1; //ERROR             
+  
+   ckps.cog = 1; //1-й зуб         
+   ckps.ignition_pulse_cogs_14+=2;
+   ckps.ignition_pulse_cogs_23+=2;
+  }
+  else
+  {
+   //Если это не синхрометка и обороты опустилисть ниже определенного порога - двигатель остановился,
+   //то переходим в режим запуска и синхронизации
+   if (ckps.period_curr > 12500) 
+   {
+    ckps.cog = 0; //мы начнем просто пропускать зубья
+    ckps.starting_mode = 0;
+    flags.ckps_is_synchronized = 0;       
+    return;
+   }
+  }
+
+synchronized_enter:     
+
+  //за 66 градусов до в.м.т перед рабочим циклом устанавливаем новый УОЗ для реализации, УОЗ
+  //до этого хранился во временном буфере.
+  if (ckps.cog == ckps.cogs_latch14)
+  {
+   //начинаем отсчет угла опережения
+   ckps.current_angle = ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * 11;
+   ckps.advance_angle = ckps.advance_angle_buffered;
+   flags.ckps_delay_prepared14 = 0;
+   //knock_start_settings_latching();    nearest future!!!
+   //adc_begin_measure();                nearest future!!!   
+  }
+  if (ckps.cog == ckps.cogs_latch23)
+  {
+   //начинаем отсчет угла опережения
+   ckps.current_angle = ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * 11;
+   ckps.advance_angle = ckps.advance_angle_buffered;
+   flags.ckps_delay_prepared23 = 0;
+   //knock_start_settings_latching();    nearest future!!!
+   //adc_begin_measure();                nearest future!!!   
+  }
+
+  
+  //диаметральные зубья завершения/начала измерения периодов вращения  - в.м.т.   
+  //считывание и сохранение измеренного периода, затем запоминаение текущего значения счетчика
+  //для следующего измерения 
+  if (ckps.cog==ckps.cogs_btdc14 || ckps.cog==ckps.cogs_btdc23) 
+  {
+   //если было переполнение то устанавливаем максимально возможное время
+   if (((ckps.period_curr > 1250) || !flags.ckps_is_valid_half_turn_period))
+    ckps.half_turn_period = 0xFFFF;
+   else
+    {     
+     ckps.half_turn_period = (GetICR() - ckps.measure_start_value);
+     flags.ckps_is_valid_half_turn_period = 1;
+    }
+   ckps.measure_start_value = GetICR();
+   /////////////////////////////////////////
+   flags.ckps_new_engine_cycle_happen = 1; //устанавливаем событие цикловой синхронизации 
+   adc_begin_measure();                    //запуск процесса измерения значений аналоговых входов
+   /////////////////////////////////////////
+  }
+
+  //подготовка к запуску зажигания для текущего канала
+  if (!flags.ckps_delay_prepared14)
+  {
+   diff = ckps.current_angle - ckps.advance_angle;
+   if (diff <= (ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * 2))
+   {
+   //до запуска зажигания осталось отсчитать меньше 2-x зубов. Необходимо подготовить модуль сравнения
+   prepare_channel14(GetICR() + ((unsigned long)diff * (ckps.period_curr)) / ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG));       
+   ckps.ignition_pulse_cogs_14 = 0;   
+   flags.ckps_delay_prepared14 = 1;
+   }
+  }
+  if (!flags.ckps_delay_prepared23)
+  {
+   diff = ckps.current_angle - ckps.advance_angle;
+   if (diff <= (ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG) * 2))
+   {
+    //до запуска зажигания осталось отсчитать меньше 2-x зубов. Необходимо подготовить модуль сравнения
+    prepare_channel23(GetICR() + ((unsigned long)diff * (ckps.period_curr)) / ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG));    
+    ckps.ignition_pulse_cogs_23 = 0;   
+    flags.ckps_delay_prepared23 = 1;
+   }
+  }
+
   if (ckps.ignition_pulse_cogs_14 >= ckps.ignition_cogs)
     TCCR1A|= (1<<FOC1B); //конец импульса запуска зажигания для цилиндров 1-4
 
   if (ckps.ignition_pulse_cogs_23 >= ckps.ignition_cogs)
     TCCR1A|= (1<<FOC1A); //конец импульса запуска зажигания для цилиндров 2-3
-      
+
+  //прошел зуб - угол до в.м.т. уменьшился на 6 град.
+  ckps.current_angle-= ANGLE_MAGNITUDE(CKPS_DEGREES_PER_COG);
   ckps.icr_prev = GetICR();
   ckps.period_prev = ckps.period_curr;  
   ckps.cog++; 
-  ckps.ignition_pulse_cogs_14++; 
-  ckps.ignition_pulse_cogs_23++; 
 }
--- knock.c
+++ knock.c
@@ -10,35 +10,117 @@
 #include <iom16.h>
 #include "knock.h"
 
+//HIP9011 - Knock Signal Processor. 
 
-void knock_set_band_pass(unsigned char freq)
-{
-}
+//SS управляет выборкой кристалла
+#define KSP_CS PORTB_Bit4
 
-void knock_set_attenuation(unsigned char attenuation)
+//Данная структура предназначена для дублирования данных текущего
+//состояния сигнального процессора.
+typedef struct
 {
-}
+ unsigned char ksp_bpf; 
+ unsigned char ksp_gain;
+ unsigned char ksp_inttime;
+ unsigned char ksp_interrupt_state;
+}KSPSTATE;
 
-void knock_set_integration_time(unsigned char inttime)
+KSPSTATE ksp;
+
+
+void spi_master_init(void);
+void spi_master_transmit(char cdata);
+
+void knock_set_integration_mode(char mode) 
 {
+ PORTD_Bit3 = mode;
 }
 
 void knock_module_initialize(void)
 {
+ char so_terminal_status = 0x01; //Hi Z
+ spi_master_init();
+ KSP_CS = 0;
+ //set clock and terminal status, clock = 4mHz
+ spi_master_transmit(0x40 | so_terminal_status);
+ //Test/Channel Select Control: Channel0 
+ spi_master_transmit(0xE0);
+ KSP_CS = 1;
+ ksp.ksp_interrupt_state = 0;
 }
 
 void spi_master_init(void)
 {
- // Set MOSI and SCK output, all others do not change 
+ //Устанавливаем MOSI и SCK как выходы, остальное не меняем 
  DDRB|= (1 << DDB5)|(1 << DDB7);
- // Enable SPI, Master, set clock rate fck/16
- SPCR = (1 << SPE)|(1 << MSTR)|(1 << SPR0);
+ // разрешаем SPI, мастер, clock = fck/16, данные по спадающему фронту SCK
+ SPCR = (1 << SPE)|(1 << MSTR)|(1 << SPR0)|(1 << CPHA);
 }
 
 void spi_master_transmit(char cdata)
 {
- // Start transmission
+ //Начало передачи
  SPDR = cdata;
- // Wait for transmission complete
+ //Ждем завершения передачи
  while(!(SPSR & (1 << SPIF)));
+}
+
+void knock_start_settings_latching(void)
+{
+ KSP_CS = 0;
+ SPDR = ksp.ksp_bpf;
+ //разрешаем прерывание, передача остальных данных будет завершена в прерывании
+ SPCR|= (1 << SPIE); 
+}
+
+unsigned char knock_is_latching_idle(void)
+{
+ return (ksp.ksp_interrupt_state) ? 0 : 1;
+}
+
+void knock_set_band_pass(unsigned char freq)
+{
+  __disable_interrupt();    
+ //00XXXXXX, 00 - два бита адреса
+ ksp.ksp_bpf = freq & 0x3F;
+ __enable_interrupt(); 
+}
+
+void knock_set_gain(unsigned char gain)
+{
+  __disable_interrupt();    
+ //10XXXXXX, 10 - два бита адреса
+ ksp.ksp_gain = (gain & 0x3F) | 0x80;
+ __enable_interrupt(); 
+}
+
+void knock_set_integration_time(unsigned char inttime)
+{
+  __disable_interrupt();    
+ //110XXXXX, 110 - три бита адреса
+ ksp.ksp_inttime = (inttime & 0x1F) | 0xC0;
+ __enable_interrupt(); 
+}
+
+#pragma vector=SPI_STC_vect
+__interrupt void spi_dataready_isr(void)
+{
+  switch(ksp.ksp_interrupt_state)
+  {
+   case 0: //BPF загружена
+    SPDR = ksp.ksp_gain;
+    ksp.ksp_interrupt_state = 1;
+    break; 
+   case 1: //Gain загружена
+    SPDR = ksp.ksp_inttime;
+    ksp.ksp_interrupt_state = 2;
+    break; 
+   case 2: //Int.Time загружено
+    //запрещаем прерывание и устанавливаем конечный автомат в состояние
+    //готовности к новой загрузке. Также выключаем сигнал выборки кристалла
+    SPCR&= ~(1 << SPIE); 
+    ksp.ksp_interrupt_state = 0;
+    KSP_CS = 1;
+    break; 
+  }
 }

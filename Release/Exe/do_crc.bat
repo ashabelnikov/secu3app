5f6496aefa93e96a7027ef424382766fd175efdc
ashabelnikov <ashabelnikov@c1dae7de-a428-49f4-85d8-50b65f41d01d>
2010-08-29 17:37:02 +0000

    Add multi platform support
    git-svn-id: http://svn2.assembla.com/svn/secu3app@132 c1dae7de-a428-49f4-85d8-50b65f41d01d

--- Release/Exe/do_crc.bat
+++ Release/Exe/do_crc.bat
@@ -5,9 +5,41 @@ rem Created by Alexey A. Shabelnikov, Kiev 26 September 2009.
 set LOGFILE=crclog.txt
 set HEXTOBIN=hextobin.exe
 set CODECRC=codecrc.exe
+set USAGE=Supported options: M16,M32,M64
+set FW_SIZE=Undefined
+set CRC_ADDR=Undefined
 
+IF "%1" == "" (
+echo Command line option required.
+echo %USAGE%
+exit 1
+)
+
+rem Check validity of command line option and set corresponding parameters
+IF %1 == M16 ( 
+set FW_SIZE=15870
+set CRC_ADDR=3DFE
+GOTO dowork
+)
+
+IF %1 == M32 ( 
+set FW_SIZE=31742
+set CRC_ADDR=7BFE
+GOTO dowork
+)
+
+IF %1 == M64 ( 
+set FW_SIZE=63486
+set CRC_ADDR=F7FE
+GOTO dowork
+)
+
+echo Invalid platform! 
+echo %USAGE%
+exit 1
+
+:dowork
 echo See %LOGFILE% for detailed information.
-
 IF EXIST %LOGFILE% del %LOGFILE%
 
 echo EXECUTING BATCH... >> %LOGFILE%
@@ -30,9 +62,9 @@ IF NOT EXIST %CODECRC% (
 echo ERROR: Can not find file "%CODECRC%" >> %LOGFILE%
 goto error
 )
-%CODECRC% secu-3_app.bin secu-3_app.a90  0  15870  3DFE -h >> %LOGFILE%
+%CODECRC% secu-3_app.bin secu-3_app.a90  0  %FW_SIZE%  %CRC_ADDR% -h >> %LOGFILE%
 IF ERRORLEVEL 1 GOTO error
-%CODECRC% secu-3_app.bin secu-3_app.bin  0  15870  3DFE -b >> %LOGFILE%
+%CODECRC% secu-3_app.bin secu-3_app.bin  0  %FW_SIZE%  %CRC_ADDR% -b >> %LOGFILE%
 IF ERRORLEVEL 1 GOTO error
 
 echo --------------------------------------------- >> %LOGFILE%

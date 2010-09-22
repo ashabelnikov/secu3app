d38c3c0ab11115f97e9dc1132418a7ebdc607fc8
ashabelnikov <ashabelnikov@c1dae7de-a428-49f4-85d8-50b65f41d01d>
2010-09-22 21:52:21 +0000

    SECU3BOOTSTART calculated incorrectly - reverted
    git-svn-id: http://svn2.assembla.com/svn/secu3app@146 c1dae7de-a428-49f4-85d8-50b65f41d01d

--- bootldr.h
+++ bootldr.h
@@ -26,7 +26,7 @@
 
 //определ€ем стартовый адрес бутлоадера в прошивке (в байтах)
 //FLASHEND определено в ioavr.h
-#define SECU3BOOTSTART (((((unsigned int)FLASHEND) + 1) * 2) - BOOT_LOADER_SIZE)
+#define SECU3BOOTSTART ((((unsigned int)FLASHEND) + 1) - BOOT_LOADER_SIZE)
 
 //точка входа в бутлоадер из программы (мину€ проверку перемычки)
 //смотрите исходный код загрузчика 

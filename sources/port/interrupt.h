c06b1ea9c47fb0de31908c6a0c25609ed4ab30e7
ashabelnikov <shabelnikov@secu-3.org>
2011-07-24 14:35:51 +0300

    Port source code to WinAvr GCC. IAR is still supported. Now Makefiles are used instead of IDE! (license has been added)

--- sources/port/interrupt.h
+++ sources/port/interrupt.h
@@ -1,3 +1,24 @@
+/* SECU-3  - An open source, free engine control unit
+   Copyright (C) 2007 Alexey A. Shabelnikov. Ukraine, Gorlovka
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   contacts:
+              http://secu-3.org
+              email: shabelnikov@secu-3.org
+*/
+
 #ifndef _SECU3_INTERRUPT_H_
 #define _SECU3_INTERRUPT_H_
 

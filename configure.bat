abb010e245bd80c43a2f0485dd5ec22af48c45c7
ashabelnikov <shabelnikov@secu-3.org>
2011-07-25 17:51:57 +0300

    Add configure.sh for Linux

--- configure.bat
+++ configure.bat
@@ -1,14 +1,15 @@
 @echo off
-rem Batch file for build firmware of SECU-3 project
-rem Created by Alexey A. Shabelnikov, Kiev 17 July 2011. 
-rem Note: It requires IAR compiler and linker
+rem Batch file for build firmware of SECU-3 project under MS Windows. This script will
+rem configure Makefile for you and build project.
+rem Created by Alexey A. Shabelnikov, Kiev 17 July 2011.
+rem Note: It requires IAR compiler and linker or WinAvr
 
 set USAGE=Supported platforms: M16,M32,M64  Supported toolchains: IAR,GCC
 set PLATFORM=Undefined
 set CFGFILE=platform_cfg
 set MAKEFILE=Undefined
 
-set CFG_MCU=Undefined 
+set CFG_MCU=Undefined
 set CFG_BL_START=Undefined
 set CFG_FWD_START=Undefined
 set CFG_EE_SIZE=Undefined
@@ -32,46 +33,46 @@ set CFG_BL_START=3E00
 set CFG_FWD_START=3263
 set CFG_EE_SIZE=512
 set CFG_LNKXCL=lnkm16s.xcl
-) else IF %1 == M32 ( 
+) else IF %1 == M32 (
 set CFG_MCU=m32
 set CFG_BL_START=7C00
 set CFG_FWD_START=7063
 set CFG_EE_SIZE=1024
 set CFG_LNKXCL=lnkm32s.xcl
-) else IF %1 == M64 ( 
+) else IF %1 == M64 (
 set CFG_MCU=m64
 set CFG_BL_START=F800
 set CFG_FWD_START=EC63
 set CFG_EE_SIZE=2048
 set CFG_LNKXCL=lnkm64s.xcl
 ) else (
-echo Invalid platform! 
+echo Invalid platform!
 echo %USAGE%
 exit 1
 )
 
-IF %2 == IAR ( 
-IF %1 == M16 ( 
+IF %2 == IAR (
+IF %1 == M16 (
 set CFG_MCU=m16
-) else IF %1 == M32 ( 
+) else IF %1 == M32 (
 set CFG_MCU=m32
-) else IF %1 == M64 ( 
+) else IF %1 == M64 (
 set CFG_MCU=m64
 )
 set MAKEFILE=Makefile_iar
 GOTO build
-) else IF %2 == GCC ( 
-IF %1 == M16 ( 
+) else IF %2 == GCC (
+IF %1 == M16 (
 set CFG_MCU=atmega16
-) else IF %1 == M32 ( 
+) else IF %1 == M32 (
 set CFG_MCU=atmega32
-) else IF %1 == M64 ( 
+) else IF %1 == M64 (
 set CFG_MCU=atmega64
 )
 set MAKEFILE=Makefile_gcc
 GOTO build
 ) else (
-echo Invalid toolchain! 
+echo Invalid toolchain!
 echo %USAGE%
 exit 1
 )
---
+++ configure.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# Script for build firmware of SECU-3 project under Linux. This script will
+# configure Makefile for you and build project.
+# Created by Alexey A. Shabelnikov, Kiev 24 July 2011.
+# Note: It requires AVR-GCC toolchain
+
+USAGE="USAGE: configure.sh <platform>  \n Supported platforms: M16,M32,M64"
+PLATFORM=Undefined
+CFGFILE="platform_cfg"
+MAKEFILE="Makefile_gcc"
+
+CFG_MCU=Undefined
+CFG_BL_START=Undefined
+CFG_FWD_START=Undefined
+CFG_EE_SIZE=Undefined
+CFG_LNKXCL=Undefined
+
+#check number of command line arguments
+if [ $# -ne 1 ]
+then
+ echo "Invalid command line arguments."
+ echo " "$USAGE
+ exit 1  #error
+fi
+
+# Check validity of command line option
+if [ $1 = "M16" ]
+then
+ CFG_MCU="atmega16"
+ CFG_BL_START="3E00"
+ CFG_FWD_START="3263"
+ CFG_EE_SIZE="512"
+elif [ $1 = "M32" ]
+then
+ CFG_MCU="atmega32"
+ CFG_BL_START="7C00"
+ CFG_FWD_START="7063"
+ CFG_EE_SIZE="1024"
+elif [ $1 = "M64" ]
+then
+ CFG_MCU="atmega64"
+ CFG_BL_START="F800"
+ CFG_FWD_START="EC63"
+ CFG_EE_SIZE="2048"
+else
+ echo "Invalid platform!"
+ echo " "$USAGE
+ exit 1  #error
+fi
+
+# Generate configuration file
+echo "MCU="$CFG_MCU > $CFGFILE
+echo "BL_START="$CFG_BL_START >> $CFGFILE
+echo "FWD_START="$CFG_FWD_START >> $CFGFILE
+echo "EE_SIZE="$CFG_EE_SIZE >> $CFGFILE
+
+#check presence of soure Makefile
+if [ ! -e "$MAKEFILE" ]
+then
+ echo "ERROR: Can not find file "$MAKEFILE
+ exit 1  #error
+fi
+make clean
+cp $MAKEFILE Makefile
+
+#start build and check for errors
+make -f $MAKEFILE
+if [ $? -ne 0 ]
+then
+ exit 1  #error
+fi
+
+#OK. All operations were completed successfully
+exit 0

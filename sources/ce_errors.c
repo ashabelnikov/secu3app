196d5884ba0c28e479930cb7f6785a9090b55bd4
ashabelnikov <shabelnikov@secu-3.org>
2012-05-03 16:44:12 +0300

    Check engine. Add special checking for THERMISTOR_CS

--- sources/ce_errors.c
+++ sources/ce_errors.c
@@ -115,8 +115,13 @@ void check(struct ecudata_t* d)
  //checking coolant temperature sensor
  if (d->param.tmp_use)
  {
+#ifndef THERMISTOR_CS
   // error if (2.28v > voltage > 3.93v)
   if (d->sens.temperat_raw < ROUND(2.28 / ADC_DISCRETE) || d->sens.temperat_raw > ROUND(3.93 / ADC_DISCRETE))
+#else
+  // error if (0.2v > voltage > 4.7v) for thermistor
+  if (d->sens.temperat_raw < ROUND(0.2 / ADC_DISCRETE) || d->sens.temperat_raw > ROUND(4.7 / ADC_DISCRETE))
+#endif
    ce_set_error(ECUERROR_TEMP_SENSOR_FAIL);
   else
    ce_clear_error(ECUERROR_TEMP_SENSOR_FAIL);

91b3ce6d21d508d3bfd84752b5b767723a590eeb
ashabelnikov <shabelnikov@secu-3.org>
2013-09-04 12:52:44 +0300

    Add COPT_ATMEGA644 compilation option

--- sources/compilopt.h
+++ sources/compilopt.h
@@ -56,6 +56,13 @@
  #define COPT_ATMEGA128 0
 #endif
 
+/**ATmega644 target */
+#ifdef _PLATFORM_M644_
+ #define COPT_ATMEGA644 1
+#else
+ #define COPT_ATMEGA644 0
+#endif
+
 /**Use VPSEM mode */
 #ifdef VPSEM
  #define COPT_VPSEM 1

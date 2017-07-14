3a80ce43e46385b4a7b8bf0cc587afc854138003
ashabelnikov <shabelnikov@secu-3.org>
2017-07-14 13:28:04 +0300

    Add rest OBD data packets needed by dashboard

--- sources/obd.c
+++ sources/obd.c
@@ -56,8 +56,8 @@ void obd_init(void)
  */
 static uint16_t calc_speed(struct ecudata_t* d)
 {
- if (d->sens.speed == 0xFFFF)
-  return 0;
+ if (d->sens.speed == 0xFFFF || d->sens.speed == 0)
+  return 0; //return 0 if speed is too low (overflow) or SPD_SENS is not mapped to real I/O
  //TODO: use 1/speed approximation instead of division, so division will be replaced by multiplication
  return ((((uint32_t)d->param.vss_period_dist) * 1098) / d->sens.speed);   //V = (period_dist * 1125000) / period_tics, 112500 = (3600 * 312500) / 1000
 }

6f5323d1ec86f662f66b35f58d7a03666f9656dc
Alexey A. Shabelnikov <shabelnikov@secu-3.org>
2019-03-03 20:27:32 +0200

    Enable Bluetooth for SECU-3i by default

--- sources/tables.c
+++ sources/tables.c
@@ -453,7 +453,7 @@ PGM_FIXED_ADDR_OBJ(fw_data_t fw_data, ".firmware_data") =
 
   .bt_flags =                    _BV(BTF_SET_BBR) | _BV(BTF_CHK_FWCRC)
 #ifndef SECU3T
-| _BV(BTF_BT_TYPE)
+| _BV(BTF_BT_TYPE) | _BV(BTF_USE_BT)
 #endif
 ,
   .ibtn_keys =                   {{0,0,0,0,0,0},{0,0,0,0,0,0}},/**<--iButton keys database. Write out your own 48-bit keys here */

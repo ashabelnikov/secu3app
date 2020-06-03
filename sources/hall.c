dff80f5b8f7494bc63b2ea6065b6465c9507e73d
Alexey A. Shabelnikov <shabelnikov@secu-3.org>
2020-06-03 14:04:20 +0300

    Fix bug with set_timer8()

--- sources/hall.c
+++ sources/hall.c
@@ -558,10 +558,13 @@ ISR(TIMER1_COMPB_vect)
  */
 static inline void set_timer0(uint16_t value)
 {
-  OCR0A = TCNT0 + _AB(value, 0);
-  hall.TCNT0_H = _AB(value, 1);
-  SETBIT(TIMSK0, OCIE0A);
-  SETBIT(TIFR0, OCF0A);
+ uint8_t TCNT0_L = _AB(value, 0);
+ if (!TCNT0_L)
+  TCNT0_L++;
+ OCR0A = TCNT0 + TCNT0_L;
+ SETBIT(TIFR0, OCF0A);
+ ckps.TCNT0_H = _AB(value, 1);
+ SETBIT(TIMSK0, OCIE0A);
 }
 
 /** Special function for processing of falling edge,

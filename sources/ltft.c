6b456ce6c5e6d8b716a00eee6954e40658a8655f
Alexey A. Shabelnikov <shabelnikov@secu-3.org>
2021-10-08 11:49:08 +0300

    Fix some issues in the LTFT code

--- sources/ltft.c
+++ sources/ltft.c
@@ -24,6 +24,8 @@
  * Implementation of the long term fuel trim algorithm
  */
 
+#ifdef FUEL_INJECT
+
 #include "port/port.h"
 #include "port/pgmspace.h"
 #include <stdlib.h>
@@ -35,22 +37,19 @@
 #include "lambda.h"
 #include "mathemat.h"
 
-#ifdef FUEL_INJECT
+#define LTFT_MIN -126 //!< Min. value in LTFT map; -126 / 512 = -0.246 (-24.6%)
+#define LTFT_MAX  126 //!< Max. value in LTFT map;  126 / 512 =  0.246 ( 24.6%)
 
-#define LTFT_MIN -126 //!< -126 / 512 = -0.246 (-24.6%)
-#define LTFT_MAX  126 //!<  126 / 512 =  0.246 ( 24.6%)
-
-/***/
+/**Number of successive switches of signal*/
 #define LAMBDA_SWT_NUM 4
 
-uint8_t ltft_state = 0;
-uint16_t stat_tmr = 0;
-uint8_t ltft_idx_r = 0;
-uint8_t ltft_idx_l = 0;
-int16_t ltft_corr = 0;
-uint8_t idx_l = 0;
-uint8_t idx_r = 0;
-int16_t ltft_curr = 0;
+uint8_t ltft_state = 0;  //!< SM state
+uint16_t stat_tmr = 0;   //!< timer
+uint8_t ltft_idx_r = 0;  //!< rpm index of current work point
+uint8_t ltft_idx_l = 0;  //!< load index of current work point
+int16_t ltft_corr = 0;   //!< value of actual correction
+uint8_t idx_l = 0;       //!< index for iteration throught load axis
+uint8_t idx_r = 0;       //!< index for iteration throught rpm axis
 
 void ltft_control(void)
 {
@@ -68,7 +67,7 @@ void ltft_control(void)
  switch(ltft_state)
  {
   case 0:
-  {
+  { //wait for work point to enter restricted band around a cell
    uint8_t r = ltft_check_rpm_hit();
    uint8_t l = ltft_check_load_hit();
    if (r != 255 && l != 255)
@@ -86,20 +85,20 @@ void ltft_control(void)
    uint8_t r = ltft_check_rpm_hit();
    uint8_t l = ltft_check_load_hit();
    if (r == 255 || l == 255)
-   {
+   { //work point came out restricted band - reset SM state
     ltft_state = 0;
     break;
    }
 
    if (((s_timer_gtc() - stat_tmr) >= PGM_GET_BYTE(&fw_data.exdata.ltft_stab_time)) && lambda_get_swt_counter() >= LAMBDA_SWT_NUM)
    {
-    ltft_curr = d.inj_ltft[l][r];
+    int16_t ltft_curr = d.inj_ltft[l][r];
     int16_t new_val = ltft_curr + d.corr.lambda;
     restrict_value_to(&new_val, LTFT_MIN, LTFT_MAX);
-    d.inj_ltft[l][r] = new_val; //apply correction to current cell
+    d.inj_ltft[l][r] = new_val;     //apply correction to current cell
     ltft_corr = new_val - ltft_curr;
-    d.corr.lambda-=ltft_corr;   //reduce current lambda by actual value of correction (taking into account possible min/max restriction)
-    ltft_idx_r = r, ltft_idx_l = l;
+    d.corr.lambda-=ltft_corr;       //reduce current lambda by actual value of correction (taking into account possible min/max restriction)
+    ltft_idx_r = r, ltft_idx_l = l; //remember indexes of current work point
     idx_l = 0, idx_r = 0;
     ltft_state++;
    }
@@ -115,8 +114,8 @@ void ltft_control(void)
    {
     int8_t dist_l = abs(ltft_idx_l - idx_l);
     int8_t dist_r = abs(ltft_idx_r - idx_r);
-    int8_t dist = (dist_l > dist_r) ? dist_l : dist_r;
-    int16_t new_val = ltft_curr + (((((int32_t)ltft_corr) * PGM_GET_BYTE(&fw_data.exdata.ltft_learn_grad)) >> 8) / dist);
+    int8_t dist = (dist_l > dist_r) ? dist_l : dist_r; //find maximum distance
+    int16_t new_val = ((int16_t)d.inj_ltft[idx_l][idx_r]) + (((((int32_t)ltft_corr) * PGM_GET_BYTE(&fw_data.exdata.ltft_learn_grad)) >> 8) / dist);
     restrict_value_to(&new_val, LTFT_MIN, LTFT_MAX);
     d.inj_ltft[idx_l][idx_r] = new_val;
    }

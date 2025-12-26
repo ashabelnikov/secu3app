ecfea7e3fb8936fa0227c649ed19e45895270af0
ashabelnikov <shabelnikov@secu-3.org>
2025-12-26 22:39:13 +0200

    Fix issue with firmware CRC

--- sources/tables.c
+++ sources/tables.c
@@ -2746,7 +2746,7 @@ PGM_FIXED_ADDR_OBJ(fw_data_t fw_data, ".firmware_data") =
   * Date in format Mmm dd yyyy.
   * Do not forget to write out same value of version into to the fw_version field of cd_data_t!
   */
- {"SECU-3 firmware v5.0. Build ["__DATE__" "__TIME__"] "},
+ {"SECU-3 firmware v5.0. Build ["__DATE__" "__TIME__"]  "},
 
  /**Version of this structure - 0.0*/
  0x00,
